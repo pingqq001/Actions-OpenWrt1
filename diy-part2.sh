7e414003ad7b55c877a4b80fe9de3f3073373fc2
pingqq001 <105734691+pingqq001@users.noreply.github.com>
2022-08-05 22:52:56 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -17,7 +17,8 @@ sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generat
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci-ssl-nginx/Makefile   # 选择argon为默认主题
 rm -rf  feeds/luci/themes/luci-theme-argon   # 删除自带argon
 
-#git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon   # 替换新版argon
 
-#git clone -b 18.06 https://github.com/r1172464137/luci-theme-edge.git package/luci-theme-edge   # 替换新版edge
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon   # 替换新版argon
+
+git clone -b 18.06 https://github.com/r1172464137/luci-theme-edge.git package/luci-theme-edge   # 替换新版edge
 
