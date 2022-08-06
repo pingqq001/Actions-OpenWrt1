adf0bffa14123718b4674861be0390ebd7c5ba89
pingqq001 <105734691+pingqq001@users.noreply.github.com>
2022-08-06 22:57:03 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,8 +18,8 @@
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.defaul
 
 
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
 echo 'src-git istore https://github.com/luochongjun/istore.git' >>feeds.conf.default
 echo 'src-git openclash https://github.com/vernesong/openclash.git' >>feeds.conf.default
